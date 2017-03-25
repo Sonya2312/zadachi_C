2656dcd04f2c5fef23b4ac557eef46f502c772ce
Sonya2312 <ira23121998@mail.ru>
2017-03-25 19:21:19 +0400

    Update 17.c

--- modul1/17.c
+++ modul1/17.c
@@ -1,25 +1,40 @@
-    #include <stdio.h>
-    #include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
 
-    void copy(char *c,char *d)
-    { char str[100];
-    FILE *C,*D;
-    C=fopen(c,"r");
-    D=fopen(d,"w");
-    char i;
-    while((i=fgetc(C))!=EOF)
-    {
-    fputc(i,D);
-    }
-    }
-    int main()
-    {
-    char a[100],b[100];
-    gets(a);
-    gets(b);
-    copy(a,b);
-    printf("Ok");
-    return 0;
-    }
 
+double binom(double * mas, int a)
+{
+	double b, c;
+	int i;
+	b = mas[0];
+	for (i = 0; i < a; i++)
+	{
+		if (b < mas[i])
+			b = mas[i];
+	}
+	c = mas[0];
+	for (i = 0; i < a; i++)
+	{
+		if (c > mas[i])
+			c = mas[i];
+	}
+	return b - c;
+}
+
+int main()
+{
+	setlocale(0, "russian");
+	int a, i;
+	double d;
+	printf("Пожалуйста, введите количество символов в массиве: ");
+	scanf("%i", &a);
+	double * mas = (double *)malloc(a*8);
+	printf("Пожалуйста, введите массив: ");
+	for (i = 0; i < a; i++)
+		scanf("%lf", &mas[i]);
+	d = binom(mas, a);
+	printf("Разность между наибольшим и наименьшим значениями: %lf\n", d);
+	free(mas);
+	return 0;
+}
 
