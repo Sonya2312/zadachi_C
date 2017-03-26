9d9d41d3e97c68d00500ff6e2660475b17ff6625
Sonya2312 <ira23121998@mail.ru>
2017-03-26 23:19:47 +0400

    Update 12.c

--- modul1/12.c
+++ modul1/12.c
@@ -1,16 +1,24 @@
-    #include <stdio.h>
+#include <stdio.h>
+#include <locale.h>
 
-    int main ( void )
-    {
-    int biggest = 0, c;
+int main()
+{
+int i;
+char sonya[100];
+for (i = 0; i< 100; i++)
+sonya[i] = '\0';
+printf("Пожалуйста, введите строчку :");
+gets(sonya);
+char c;
+c = sonya[0];
+for (i = 0; i < 100; i++)
+{
+if (c < sonya[i])
+{
+c = sonya[i];
+}
+}
+printf("Наибольший ASCII код у символа %c\n", c);
 
-    while ((c = getchar()) != EOF) {
-    if ( c > biggest)
-    biggest = c;
-    }
-
-    printf("%s%c%3d\n","The symbol whith biggest ASCII code number is ", biggest, biggest);
-    return 0;
-    }
-
-
+return 0;
+}
