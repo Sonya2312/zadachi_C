07937242529e0d704400884a1497730db03ec6b1
Sonya2312 <ira23121998@mail.ru>
2017-03-26 23:10:03 +0400

    Update 9.c

--- modul1/9.c
+++ modul1/9.c
@@ -16,4 +16,15 @@ printf("\n");
 for(int i=1; i<n+1; i++){
 sonya = fib(i);
 printf("%d ", sonya);
-}}
+}
+printf("\n");
+return 0;
+}
+
+int fib(int n){
+int answer;
+if(n==1) return(1);
+if(n==2) return(1);
+answer = fib(n-2) + fib(n-1);
+return(answer);
+}
