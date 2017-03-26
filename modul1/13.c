d6575d25865eec96b04abf86073de79da721c01e
Sonya2312 <ira23121998@mail.ru>
2017-03-26 23:28:13 +0400

    Update 13.c

--- modul1/13.c
+++ modul1/13.c
@@ -1,36 +1,30 @@
     #include <stdio.h>
-    #include <ctype.h>
 
-    int main()
-    {
-    int c, i, j, nc;
-    i=j=nc=0;
-    int dig[17];
+int main()
+{
+char sonya[100];
+int vlad = 0;
 
-    for(i=0; i<17; i++)
-    dig[i] = 0;
-    while((c=getchar()) != EOF)
-    {
-    nc++;
-    if(c==' ' || c=='\n' || c=='\t' || ispunct(c))
-    {
-    dig[nc-1]++;
-    nc=0;
-    }
-    }
-    for(i=1; i<17; i++) 
-    {
-    if(i<10)
-    printf(" %d ", i);
-    else
-    printf("%d ", i);
-    for(j=dig[i]; j>0; j--)
-    printf("*");
-    printf("\n");
-    }
-    printf("\n");
-    return 0;
-    }
-
-
-
+gets(sonya);
+for (int i = 0; i < 100; i++)
+{
+if ( (sonya[i] == ' ') || (sonya[i] == '\0') )
+{
+for (int c = 0; c < vlad; c++)
+printf("-");
+printf("\n");
+for (int c = 0; c < vlad - 1; c++)
+printf(" ");
+printf("|\n");
+for (int c = 0; c < vlad; c++)
+printf("-");
+printf("\n");
+vlad = 0;
+}
+else
+vlad++;
+if (sonya[i] == '\0')
+break;
+}
+return 0;
+}
